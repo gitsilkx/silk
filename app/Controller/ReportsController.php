7d8588fd93c6447ec0036c127739ff13ebc50147
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-07-14 13:05:17 +0000

    commit

--- app/Controller/ReportsController.php
+++ app/Controller/ReportsController.php
@@ -5405,7 +5405,8 @@ class ReportsController extends AppController {
          $hotel_approved_cnt = $this->TravelHotelLookup->find('count', array('conditions' => array('OR' => $conArray,'province_id !=' => '0',
              'suburb_id !=' => '0','area_id !=' => '0','chain_id !=' => '0','brand_id !=' => '0','status' => '2')));
          $hotel_total_cnt = $this->TravelHotelLookup->find('count', array('conditions' => array('OR' => $conArray)));
-         $log = $this->TravelHotelLookup->getDataSource()->getLog(false, false);       
+         $log = $this->TravelHotelLookup->getDataSource()->getLog(false, false);   
+         
             debug($log); 
         }
         
