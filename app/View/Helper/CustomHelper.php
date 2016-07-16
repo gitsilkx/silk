b4f1d34241d5bee9516c56a3926336a5d46ca198
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-07-16 12:37:31 +0000

    commit

--- app/View/Helper/CustomHelper.php
+++ app/View/Helper/CustomHelper.php
@@ -163,10 +163,10 @@ class CustomHelper extends Helper {
                         'alias' => 'TravelHotelRoomSupplier',
                         'type'  => 'LEFT',
                         'foreignKey'    => false,
-                        'conditions'    => array('NOT'=> array('TravelHotelRoomSupplier.hotel_supplier_status' => array('1','2','7')),'TravelHotelLookup.id = TravelHotelRoomSupplier.hotel_id'),
+                        'conditions'    => array('TravelHotelLookup.id = TravelHotelRoomSupplier.hotel_id'),
                         ),
                 )                   
-            ,'conditions' => array('TravelHotelLookup.status' => array('2','8'),'TravelHotelLookup.country_id' => $country_id,'TravelHotelLookup.city_id' => $city_id,'TravelHotelLookup.province_id !=' => '0',
+            ,'conditions' => array('NOT'=> array('TravelHotelRoomSupplier.hotel_supplier_status' => array('1','2','7')),'TravelHotelLookup.status' => array('2','8'),'TravelHotelLookup.country_id' => $country_id,'TravelHotelLookup.city_id' => $city_id,'TravelHotelLookup.province_id !=' => '0',
              'TravelHotelLookup.suburb_id !=' => '0','TravelHotelLookup.area_id !=' => '0','TravelHotelLookup.chain_id !=' => '0','TravelHotelLookup.brand_id !=' => '0')));
      
     }
