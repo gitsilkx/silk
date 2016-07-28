5c0e019014a8c8daede724fa96b81af61487dbff
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-07-28 05:10:42 +0000

    commit

--- app/Controller/ReportsController.php
+++ app/Controller/ReportsController.php
@@ -5666,75 +5666,41 @@ class ReportsController extends AppController {
         $TravelCountries = array();
         $Provinces= array();
         $search_condition = array();
-        $continent_id = '2';
-        $country_id = '220';
-        $province_id = '2';
-        $city_id = '1256';
+        $continent_id = '';
+        $country_id = '';
+        $province_id = '';
+        $city_id = '';
         $flag = 0;
+       
+        
 
         if ($this->request->is('post') || $this->request->is('put')) {
             
             
             if (!empty($this->data['Report']['continent_id'])) {
                 $continent_id = $this->data['Report']['continent_id'];
-                
+                array_push($search_condition, array('TravelHotelLookup.continent_id' => $continent_id));
+                $TravelCountries = $this->TravelCountry->find('list', array('fields' => 'id, country_name', 'conditions' => array('TravelCountry.continent_id' => $continent_id,
+                        'TravelCountry.country_status' => '1',
+                        'TravelCountry.wtb_status' => '1',
+                        'TravelCountry.active' => 'TRUE'), 'order' => 'country_name ASC'));
             }
 
             if (!empty($this->data['Report']['country_id'])) {
                 $country_id = $this->data['Report']['country_id'];
                 $province_id = $this->data['Report']['province_id'];
-                
+                array_push($search_condition, array('TravelHotelLookup.country_id' => $country_id));
+                $TravelCities = $this->TravelCity->find('list', array('fields' => 'id, city_name', 'conditions' => array('TravelCity.province_id' => $province_id,
+                        'TravelCity.city_status' => '1',
+                        'TravelCity.wtb_status' => '1',
+                        'TravelCity.active' => 'TRUE',), 'order' => 'city_name ASC'));
                 
                 
             }
             if (!empty($this->data['Report']['province_id'])) {
                 
                 array_push($search_condition, array('TravelHotelLookup.province_id' => $province_id));
-               
-            }
-            if (!empty($this->data['Report']['city_id'])) {
-                $city_id = $this->data['Report']['city_id'];
-                //array_push($search_condition, array('TravelHotelLookup.city_id' => $city_id));
-            }
-            
-            
-        }
-       
-        elseif ($this->request->is('get')) {
-
-           
-
-            if (!empty($this->request->params['named']['continent_id'])) {
-                $continent_id = $this->request->params['named']['continent_id'];
-                
-            }
-
-            if (!empty($this->request->params['named']['country_id'])) {
-                $country_id = $this->request->params['named']['country_id'];
-                $province_id = $this->request->params['named']['province_id'];
-                
-            }
-            if (!empty($this->request->params['named']['province_id'])) {
-
-                
-                
-            }
-
-            if (!empty($this->request->params['named']['city_id'])) {
-                $city_id = $this->request->params['named']['city_id'];
-                
-                
-            }
-            
-        } 
-        
-        
-                $TravelCountries = $this->TravelCountry->find('list', array('fields' => 'id, country_name', 'conditions' => array('TravelCountry.continent_id' => $continent_id,
-                        'TravelCountry.country_status' => '1',
-                        'TravelCountry.wtb_status' => '1',
-                        'TravelCountry.active' => 'TRUE'), 'order' => 'country_name ASC'));
-                
-                 $Provinces = $this->Province->find('list', array(
+                $Provinces = $this->Province->find('list', array(
                 'conditions' => array(
                     'Province.country_id' => $country_id,
                     'Province.continent_id' => $continent_id,
@@ -5746,20 +5712,66 @@ class ReportsController extends AppController {
                 'fields' => array('Province.id', 'Province.name'),
                 'order' => 'Province.name ASC'
             ));
-                $TravelCities = $this->TravelCity->find('list', array('fields' => 'id, city_name', 'conditions' => array('TravelCity.province_id' => $province_id,
-                        'TravelCity.city_status' => '1',
-                        'TravelCity.wtb_status' => '1',
-                        'TravelCity.active' => 'TRUE',), 'order' => 'city_name ASC'));
-                
-                
+            }
+            if (!empty($this->data['Report']['city_id'])) {
+                $city_id = $this->data['Report']['city_id'];
+                array_push($search_condition, array('TravelHotelLookup.city_id' => $city_id));
+            }
+            
+          $flag = 1;
+        }
+        
+        elseif (count($this->request->params['named'])) {
+
+           //die;
+
+            if (!empty($this->request->params['named']['continent_id'])) {
+                $continent_id = $this->request->params['named']['continent_id'];
                 array_push($search_condition, array('TravelHotelLookup.continent_id' => $continent_id));
+                $TravelCountries = $this->TravelCountry->find('list', array('fields' => 'id, country_name', 'conditions' => array('TravelCountry.continent_id' => $continent_id,
+                        'TravelCountry.country_status' => '1',
+                        'TravelCountry.wtb_status' => '1',
+                        'TravelCountry.active' => 'TRUE'), 'order' => 'country_name ASC'));
+            }
+
+            if (!empty($this->request->params['named']['country_id'])) {
+                $country_id = $this->request->params['named']['country_id'];
+                $province_id = $this->request->params['named']['province_id'];
                 array_push($search_condition, array('TravelHotelLookup.country_id' => $country_id));
-        array_push($search_condition, array('TravelHotelLookup.province_id' => $province_id));
-        array_push($search_condition, array('TravelHotelLookup.city_id' => $city_id));
-        $this->paginate['order'] = array('TravelHotelLookup.city_code' => 'asc');
+                $TravelCities = $this->TravelCity->find('list', array('fields' => 'id, city_name', 'conditions' => array('TravelCity.province_id' => $province_id,
+                       ), 'order' => 'city_name ASC'));
+            }
+            if (!empty($this->request->params['named']['province_id'])) {
+
+                array_push($search_condition, array('TravelHotelLookup.province_id' => $province_id));
+                $Provinces = $this->Province->find('list', array(
+                'conditions' => array(
+                    'Province.country_id' => $country_id,
+                    'Province.continent_id' => $continent_id,
+                    'Province.status' => '1',
+                    'Province.wtb_status' => '1',
+                    'Province.active' => 'TRUE',
+                    
+                ),
+                'fields' => array('Province.id', 'Province.name'),
+                'order' => 'Province.name ASC'
+            ));
+            }
+
+            if (!empty($this->request->params['named']['city_id'])) {
+                $city_id = $this->request->params['named']['city_id'];
+                array_push($search_condition, array('TravelHotelLookup.city_id' => $city_id));
+                
+            }
+           $flag = 1;
+        }   
+        
+        
+        if($flag == 1){
+         $this->paginate['order'] = array('TravelHotelLookup.city_code' => 'asc');
             $this->paginate['limit'] = '50';
             $this->set('TravelHotelLookups', $this->paginate("TravelHotelLookup", $search_condition));
-        
+        }
       
         
         if (!isset($this->passedArgs['continent_id']) && empty($this->passedArgs['continent_id'])) {
