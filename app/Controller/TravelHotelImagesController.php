4f9b6ee19279de7957e299108ef7eafda4b647ce
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-08-17 05:53:45 +0000

    commit

--- app/Controller/TravelHotelImagesController.php
+++ app/Controller/TravelHotelImagesController.php
@@ -845,9 +845,9 @@ class TravelHotelImagesController extends AppController {
                 $order_return = $client->__doRequest($xml_string, $location_URL, $action_URL, 1);
                 //$xml_arr = Xml::toArray(Xml::build($order_return));
                 $xml_arr = $this->xml2array($order_return);
-                 echo htmlentities($xml_string);
-                 pr($xml_arr);
-                 die;
+                 //echo htmlentities($xml_string);
+                 //pr($xml_arr);
+                 //die;
 
                 if ($xml_arr['SOAP:ENVELOPE']['SOAP:BODY']['PROCESSXMLRESPONSE']['PROCESSXMLRESULT']['RESOURCEDATA_HOTEL']['RESPONSEAUDITINFO']['RESPONSEINFO']['RESPONSEID'][0] == '201') {
                     $log_call_status_code = $xml_arr['SOAP:ENVELOPE']['SOAP:BODY']['PROCESSXMLRESPONSE']['PROCESSXMLRESULT']['RESOURCEDATA_HOTEL']['RESPONSEAUDITINFO']['RESPONSEINFO']['RESPONSEID'][0];
