806139d096310586c533c405ad8c2d29745e1140
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-08-20 13:22:53 +0000

    commit

--- app/Controller/TravelHotelImagesController.php
+++ app/Controller/TravelHotelImagesController.php
@@ -468,14 +468,7 @@ class TravelHotelImagesController extends AppController {
         $ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
         $login = ftp_login($ftp_conn, 'imageius@prop-genie.com', '_$g6_ZLuH&p@');
 
-        $structure = 'http://imageius.com/uploads/hotels/depth1/depth2/depth3/';
-
-// To create the nested structure, the $recursive parameter 
-// to mkdir() must be specified.
-
-if (!mkdir($structure, 0777, true)) {
-    die('Failed to create folders...');
-}
+  
 
         $TravelCountries = array();
         $TravelCities = array();
