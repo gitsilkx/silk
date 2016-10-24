52b21af7861d5f9e2a8acc82c459360fe3bc0f72
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-10-24 07:09:15 +0000

    Added again LookupResponseIssue to index compact.

--- app/Controller/SupportTicketsController.php
+++ app/Controller/SupportTicketsController.php
@@ -238,7 +238,7 @@ class SupportTicketsController extends AppController {
 
 
 
-        $this->set(compact('LookupScreen','sql_generate','update', 'solution','selected', 'TravelLookupContinent', 'TravelCountries', 'Provinces', 'TravelSuburbs', 'TravelCities', 'TravelAreas', 'LookupQuestion', 'LookupTicketUrgency', 'users', 'LookupTicketStatus'));
+        $this->set(compact('LookupScreen','sql_generate','update', 'solution','selected', 'TravelLookupContinent', 'TravelCountries', 'Provinces', 'TravelSuburbs', 'TravelCities', 'TravelAreas', 'LookupQuestion', 'LookupTicketUrgency', 'users', 'LookupTicketStatus', 'LookupResponseIssue'));
     }
 
     public function add($screen = null, $action_id = null) {
