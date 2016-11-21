0355f142ad9ef2d0f11556840e2696fe8e226cbb
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-11-21 16:53:45 +0000

    CR 3 -> Reorts.

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -124,7 +124,7 @@ class AppController extends Controller {
             'get_travel_city_by_province','get_city_code_by_province_id','get_all_travel_country_by_continent_id','get_all_travel_city_by_province',
             'get_all_travel_suburb_by_country_id_and_city_id','get_all_travel_area_by_suburb_id','get_list_question_by_question_id','ajax_get_travel_country_by_continent_id',
             'ajax_get_province_by_country_id','ajax_get_travel_city_by_province','ajax_get_travel_suburb_by_city','ajax_get_travel_area_by_suburb',
-            'get_all_country_by_continent_id','get_all_city_by_country_id','get_supplier_city_by_country_id','ajax_get_travel_brand_by_chain','get_supplier_city_code','get_travel_table_by_type_id','generate_sequence_no','get_user_list_by_summary_type'
+            'get_all_country_by_continent_id','get_all_city_by_country_id','get_supplier_city_by_country_id','ajax_get_travel_brand_by_chain','get_supplier_city_code','get_travel_table_by_type_id','generate_sequence_no','get_user_list_by_summary_type','my_activity_report'
             ));
     }
 
