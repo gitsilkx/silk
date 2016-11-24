468130c0c049d906f26a098a3c6a2f1fc91cc53a
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-11-24 08:01:51 +0000

    CR 3 -> Passed Args.

--- app/Controller/TravelActionItemsController.php
+++ app/Controller/TravelActionItemsController.php
@@ -229,10 +229,7 @@ $result_array = ClassRegistry::init('TravelHotelLookup')->find('all', array('fie
 
 array_push($search_condition, $conditions);
 
-array_push($search_condition, array('TravelActionItem.created_by_id' => $get_creator));
-array_push($search_condition, array('TravelActionItem.next_action_by' => $get_user_id));
-array_push($search_condition, array('TravelActionItem.level_id' => $get_level_id));
-array_push($search_condition, array('TravelActionItem.action_item_active' => 'Yes'));
+
 
 
 
