252ed7e48f5d1d34e69a0f3b1723f162a0374535
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-11-24 17:35:53 +0000

    CR 3 -> Direct Display.

--- app/Controller/SupportTicketsController.php
+++ app/Controller/SupportTicketsController.php
@@ -443,7 +443,7 @@ $result_array = ClassRegistry::init('TravelHotelLookup')->find('all', array('fie
 
 array_push($search_condition, $conditions);
 
-array_push($search_condition, array('SupportTicket.status' => 1));
+array_push($search_condition, array('SupportTicket.status' => '1' OR '2'));
 
 //array_push($search_condition, array('SupportTicket.city_id' => $city_id));
 
