c6d63929433aee2363b8895b334b16440570c2d6
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-11-28 17:14:14 +0000

    Pappu's version Supplier Status OR Clause ... Merged

--- app/Controller/MappingeAreasController.php
+++ app/Controller/MappingeAreasController.php
@@ -268,11 +268,11 @@ $TravelCitySuppliers = $this->TravelCitySupplier->find('all', ARRAY('conditions'
 
 // THIS PART HAS THE SUPPLIER HOTEL DATA PULLING LOGIC
 $display = 'TRUE';
-$supplier_city_codde = $this->TravelCitySupplier->find('list', ARRAY('fields' => 'supplier_city_code', 'conditions' =>
+$supplier_city_codex = $this->TravelCitySupplier->find('list', ARRAY('fields' => 'supplier_city_code', 'conditions' =>
        array('supplier_id' => $get_supplier_id, 'city_country_id' => $get_country_id, 'city_id' => $get_city_id, 'wtb_status' => '1', 'active' => 'TRUE', 'city_supplier_status' => '2', 'excluded' => 'FALSE')));
                 //pr($supplier_city_codde);
 
-       array_push($search_condition, array('SupplierHotel.city_code' => $supplier_city_codde));
+       array_push($search_condition, array('SupplierHotel.city_code' => $supplier_city_codex));
        //array_push($search_condition, array('SupplierHotel.status' => array('1','5') ));  
        array_push($search_condition, array('SupplierHotel.status' => '1'));         
        //array_push($search_condition, array('SupplierHotel.status' => '1' OR '5'));      
