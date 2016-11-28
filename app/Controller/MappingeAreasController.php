3ef915b085552c39f07768fccc783256a21f1441
biswajit <biswajit@instance-1.c.neat-height-132711.internal.>
2016-11-28 17:40:32 +0000

    Pappu's version Supplier Status OR Clause ... Merged

--- app/Controller/MappingeAreasController.php
+++ app/Controller/MappingeAreasController.php
@@ -273,7 +273,7 @@ $supplier_city_codex = $this->TravelCitySupplier->find('list', ARRAY('fields' =>
                 //pr($supplier_city_codde);
 
        array_push($search_condition, array('SupplierHotel.city_code' => $supplier_city_codex));
-       array_push($search_condition, array('SupplierHotel.status' => array('3','4') ));  
+       array_push($search_condition, array('SupplierHotel.status' => array('3','4') ));   
        //array_push($search_condition, array('SupplierHotel.status' => '1'));         
        //array_push($search_condition, array('SupplierHotel.status' => '1' OR '5'));      
        $this->paginate['order'] = array('SupplierHotel.id' => 'asc');
